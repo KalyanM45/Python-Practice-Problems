0fa639c2595b681e231f9151dfc51df3c0e41d56
Hema Kalyan Murapaka <101493756+KalyanMurapaka45@users.noreply.github.com>
2023-11-03 11:23:38 +0530

    Delete 1. Linear Search directory

--- 1. Linear Search/Linear_Search.py
+++
@@ -1,8 +0,0 @@
-def linear_search(list, key):
-    for i in range(len(list)):
-        if list[i]==key:
-            return i
-        
-list = [1,2,3,4,5,6]
-key = 5
-print(linear_search(list, key))
